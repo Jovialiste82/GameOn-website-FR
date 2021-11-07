eb6e7a32ab636b2a74642fd4e547717ec56c1f8e
Jovialiste82 <52076767+Jovialiste82@users.noreply.github.com>
2021-11-07 23:17:44 +0000

    width fix

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,24 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+closeBtn.addEventListener("click", closeModal);
